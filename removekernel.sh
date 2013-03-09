4ec63d91536d843076deee5b422d82ac26ade28d
Mark Rijckenberg <markrijckenberg@gmail.com>
2013-03-09 12:39:16 +0100

    Added -y to apt-get command to fix the removekernel.sh script.

--- removekernel.sh
+++ removekernel.sh
@@ -12,9 +12,9 @@ echo "Please enter kernelversion to remove from your pc (for example: 2.6.24-16)
 read KERNELVERSION
 
 echo "Removing kernelversion $KERNELVERSION"
-apt-cache search $KERNELVERSION|cut -d" " -f1|xargs sudo apt-get remove 
+apt-cache search $KERNELVERSION|cut -d" " -f1|xargs sudo apt-get remove -y
 
 else
 echo "Removing kernelversion $1"
-apt-cache search $1|cut -d" " -f1|xargs sudo apt-get remove
+apt-cache search $1|cut -d" " -f1|xargs sudo apt-get remove -y
 fi
