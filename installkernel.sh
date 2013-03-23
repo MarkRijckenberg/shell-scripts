f3627e4dd826c23614c98fdd27bd3f144687517b
MarkRijckenberg <markrijckenberg@gmail.com>
2013-03-23 15:11:33 +0100

    Update installkernel.sh

--- installkernel.sh
+++ installkernel.sh
@@ -1,3 +1,14 @@
+# TYPE: Bash Shell script.
+# PURPOSE: This bash shell script allows you to easily install new GNU/Linux kernels. The script should be copied to /usr/bin/installkernel and set as executable.
+# REQUIRES: ksh, Ubuntu 12.04 LTS or newer
+# REVISED: 20130323
+# AUTHOR: markrijckenberg@gmail.com
+
 sudo apt-get update
 sudo apt-get install python-bs4
-cd /tmp; rm -rf medigeek-kmp*; wget --no-check-certificate https://github.com/medigeek/kmp-downloader/tarball/master -O kmpd.tar.gz; tar xzf kmpd.tar.gz; cd medigeek-*; python kmpd.py -d
+cd /tmp
+rm -rf medigeek-kmp*
+wget --no-check-certificate https://github.com/medigeek/kmp-downloader/tarball/master -O kmpd.tar.gz
+tar xzf kmpd.tar.gz
+cd medigeek-*
+python kmpd.py -d
