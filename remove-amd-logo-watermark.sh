e3f0cf854727f18a05045099d0e425fbb1232cc4
Mark Rijckenberg <markrijckenberg@gmail.com>
2013-04-03 20:14:24 +0200

    added remove-amd-logo-watermark.sh file

---
+++ remove-amd-logo-watermark.sh
@@ -0,0 +1,23 @@
+# TYPE: Korn Shell script.
+# PURPOSE: This korn shell script allows to easily remove AMD/ATI Watermark in AMD graphics drivers for Ubuntu 13.04
+# REQUIRES: ksh, Ubuntu 13.04 or newer
+# REVISED: 20130403
+# AUTHOR: http://www.youtube.com/user/midfingr
+# UPDATED BY: markrijckenberg@gmail.com
+
+#!/bin/sh
+DRIVER=`find /usr/lib -name fglrx_drv.so`
+
+echo "Come on AMD!"
+
+for token in $DRIVER;do
+	echo "Removing AMD logo from "$token
+
+	for x in $(objdump -d $DRIVER|awk '/call/&&/EnableLogo/{print "\\x"$2"\\x"$3"\\x"$4"\\x"$5"\\x"$6}'); do
+		sed -i "s/$x/\x90\x90\x90\x90\x90/g" $DRIVER
+	done
+done
+
+echo "Reboot computer to finish"
+
+
