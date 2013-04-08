d70b569fd8ba5e3b91edb55509e30db6bf87fc60
MarkRijckenberg <markrijckenberg@gmail.com>
2013-04-08 21:26:07 +0300

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -112,3 +112,4 @@ echo "http://www.usno.navy.mil/USNO/astrometry/information/catalog-info/catalog-
 echo "http://www.usno.navy.mil/USNO/astrometry/optical-IR-prod/icas/fchpix"
 echo "http://be.kompass.com/live/fr/w2866018/edition-livres/edition-livres-astronomie-geodesie-meteorologie-1.html#.UV1dCqA9RZc"
 echo "Check with book publisher in which format the digitized book needs to be sent to the publisher (Word, ODF, hybrid PDF-ODF format, etc...)"
+echo "Also get MIPAV imaging tool here:  http://mipav.cit.nih.gov/download.php"
