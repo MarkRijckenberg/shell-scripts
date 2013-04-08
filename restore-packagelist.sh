acfb6da29e741df8edf79154c5c89d5c7c071d22
MarkRijckenberg <markrijckenberg@gmail.com>
2013-04-08 23:31:08 +0300

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -32,6 +32,8 @@ sudo apt-key add Repo.keys
 #sudo dpkg --set-selections < $TARGET1/Package.list
 #sudo dpkg --set-selections < Package.list
 #sudo dselect
+sudo cp /etc/apt/sources.list  /etc/apt/sources.list.backup
+sudo cp sources.list /etc/apt/sources.list
 sudo add-apt-repository ppa:razor-qt/ppa
 # refresh list of available packages in Ubuntu repositories
 sudo apt-get update
