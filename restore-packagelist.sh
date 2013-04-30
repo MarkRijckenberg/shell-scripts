20f0727135c41f93795f1756e8857778d62953c7
MarkRijckenberg <markrijckenberg@gmail.com>
2013-04-30 17:34:39 +0300

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -3,7 +3,7 @@
 # REQUIRES: Ubuntu 12.04.02 LTS 64-bit (to support UEFI+SecureBoot), wget, apt-get, unp, wine
 # Author: Mark Rijckenberg
 # Copyright (c) 2012-08-12
-# REVISION DATE: 20130422
+# REVISION DATE: 20130430
 # Updated by: markrijckenberg at gmail dot com
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 #Prerequisites: USB drives SAMSUNG and IOMEGA need to be mounted correctly in order for this script to work correctly!
