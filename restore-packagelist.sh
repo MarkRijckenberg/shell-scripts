3b629deb35c5a2f7a9bd345e7c53f8cc5974cff2
MarkRijckenberg <markrijckenberg@gmail.com>
2013-05-08 13:40:09 +0300

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -1,4 +1,4 @@
-# !!!!! WINDOWS users should use ninite.com for semi-automated Windows deployments
+# !!!!! WINDOWS users should use ninite.com for semi-automated Windows application deployments
 # TYPE: Bash Shell script.
 # PURPOSE: This bash shell script allows you to easily restore Ubuntu packages into a clean install of Ubuntu 12.04.02
 # REQUIRES: Ubuntu 12.04.02 LTS 64-bit (to support UEFI+SecureBoot), wget, apt-get, unp, wine
