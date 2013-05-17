a290a68e70cb9f510fb2f8c1d78cebb00cca2c16
MarkRijckenberg <markrijckenberg@gmail.com>
2013-05-17 12:30:43 +0200

    Update removekernel.bash

--- removekernel.bash
+++ removekernel.bash
@@ -5,7 +5,7 @@
 # It is a poor man's alternative to Ubuntu Tweak.
 # REQUIRES: bash, Ubuntu 12.04 LTS or newer
 # REVISED: 20130302
-# REVISED: 20130502
+# REVISED: 20130516
 # AUTHOR: mark rijckenberg at gmail dot com
 
    echo "Please press one of the 4 numbers below and then hit the <ENTER> key to execute"
