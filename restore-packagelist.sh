516fb7ad880bea6ca12be9e8190dffe5cba1c71f
MarkRijckenberg <markrijckenberg@gmail.com>
2013-06-27 14:17:17 +0200

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -13,7 +13,7 @@ PATH=/usr/sbin:/usr/bin:/sbin:/bin
 MACHINE_TYPE=`uname -m`
 
 # define basepackage filename variables - VERSION NUMBERS FREQUENTLY CHANGE!!
-TEXSTUDIOFILENAME=texstudio_2.6.0
+TEXSTUDIOFILENAME=texstudio_2.6.1
 SKYPEFILENAME=skype-ubuntu-quantal_4.2.0.11-1_i386.deb
 YEDFILENAME=yEd-3.10.2_32-bit_setup.sh
 
