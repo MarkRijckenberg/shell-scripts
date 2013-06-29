a9f8f4c874c9a7370177f4e5db8122c12e3b159e
MarkRijckenberg <markrijckenberg@gmail.com>
2013-06-29 12:48:43 +0200

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -4,6 +4,7 @@
 # PURPOSE: This bash shell script allows you to easily restore Ubuntu packages into a clean install of Ubuntu 12.10
 # REQUIRES: Ubuntu 12.10 LTS 64-bit (to support UEFI+SecureBoot+biber+bibtex), wget, apt-get, unp, wine, biber, biblatex
 # REQUIRED FREE DISKSPACE FOR BASEPACKAGES:  7 GB of free disk space in root partition
+# INSTALLDURATION: 1h24min
 # Author: Mark Rijckenberg
 # Copyright (c) 2012-08-12
 # REVISION DATE: 20130521
