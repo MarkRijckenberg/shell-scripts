55e97e3f88e5f84523aeaa3a0d7d641c9ef78c54
MarkRijckenberg <markrijckenberg@gmail.com>
2013-11-06 14:29:33 +0100

    Update restore-packagelist.sh

--- restore-packagelist.sh
+++ restore-packagelist.sh
@@ -6,7 +6,7 @@
 # PURPOSE: This bash shell script allows you to easily restore Lubuntu packages into a clean install of Lubuntu 13.10
 # REQUIRES: Lubuntu 13.10 64-bit (to support UEFI+SecureBoot+biber+bibtex), wget, apt-get, unp, wine, biber, biblatex
 # Do not use Ubuntu's Unity interface, because it causes Teamviewer sessions to slow down due to window animation in Unity
-# REQUIRED FREE DISKSPACE FOR BASEPACKAGES:  7 GB of free disk space in root partition
+# REQUIRED FREE DISKSPACE FOR BASEPACKAGES:  10 GB of free disk space in root partition
 # INSTALL DURATION: Around 1h30min
 # Author: Mark Rijckenberg
 # Copyright (c) 20120812
