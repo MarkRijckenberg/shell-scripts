90c50506a67d30fbcdc60281d98eceb5945d20d3
MarkRijckenberg <markrijckenberg@gmail.com>
2014-01-30 15:59:19 +0100

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -30,7 +30,7 @@ PATH=/usr/sbin:/usr/bin:/sbin:/bin
 MACHINE_TYPE=`uname -m`
 
 # define basepackage filename variables - VERSION NUMBERS FREQUENTLY CHANGE!!
-SKYPEFILENAME="skype-ubuntu-precise_4.2.0.11-1_i386.deb"
+SKYPEFILENAME="skype-ubuntu-precise_4.2.0.13-1_i386.deb"
 YEDFILENAME="yEd-3.11.1_32-bit_setup.sh"
 
 # define Astronomy filename variables
