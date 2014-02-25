7806d9d143999b6b2577132d714c16356c58d359
MarkRijckenberg <markrijckenberg@gmail.com>
2014-02-25 23:12:33 +0100

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -920,6 +920,8 @@ echo "http://dc.zah.uni-heidelberg.de/ppmxl/q/cone/form"
 echo "http://vizier.u-strasbg.fr/viz-bin/VizieR?-source=%20PPMXL"
 # following URL added on December 9, 2013:
 echo "http://www.pnas.org/content/by/section/Astronomy"
+# following URL added on February 25, 2014:
+echo "http://www.plos.org/publications/journals/"
 # following URL added on January 25, 2014:
 echo "http://science.time.com/category/space-2/"
 # following URL added on February 5, 2014:
