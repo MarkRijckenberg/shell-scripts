548dfff9e644e6c321d1bf036d92ab99fdde42b3
MarkRijckenberg <markrijckenberg@gmail.com>
2014-02-26 12:24:20 +0100

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -1019,7 +1019,8 @@ echo "Get more information about latex package by using command   texdoc <packag
 # Reproducible, scientific, open-source report sharing procedure:
 # 1) install R, RStudio, texmaker and knitr
 # 2) Create .Rnw document using texmaker+knitr - knitr is better than Sweave
-# 3) Share new .Rnw document and source .csv datafiles on https://github.com/
+# 3) Add an explanatory comment after each line of R code inside the .Rnw document
+# 4) Share new .Rnw document and source .csv datafiles on https://github.com/
 # knitr install procedure:
 # git clone https://github.com/yihui/knitr.git
 # R CMD build knitr
