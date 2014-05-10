781de1ac6cb297e16b0e3bad0471bcbd2c8e5fd8
MarkRijckenberg <markrijckenberg@gmail.com>
2014-05-10 14:34:10 +0200

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -141,7 +141,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install razorqt ra
 # Install lxqt desktop environment => merge of lxde and razorqt desktops
 sudo DEBIAN_FRONTEND=noninteractive add-apt-repository --yes ppa:lubuntu-dev/lubuntu-daily
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes remove  lxinput-qt 
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install  liblxqt0 libqtxdg0 libqtxdg-data 
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install  lxqt-metapackage pcmanfm-qt  lxsession lximage-qt   lxrandr-qt
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install  lxqt-about  lxqt-appswitcher  lxqt-config   lxqt-lightdm-greeter  
