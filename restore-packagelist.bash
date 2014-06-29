4b1e31875d4d92af5c6354a46a58bbb231e1182e
MarkRijckenberg <markrijckenberg@gmail.com>
2014-06-29 13:12:00 +0200

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -187,6 +187,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge blueman
 ##########################################################################################################
 cd $HOME
 sudo dpkg -P icaclient
+rm -rf $HOME/foo
 sudo dpkg --add-architecture i386 # only needed once
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install firefox apt-file git nspluginwrapper lib32z1 libc6-i386 libxml2:i386 libstdc++6:i386 libxerces-c3.1:i386 libcanberra-gtk-module:i386 libcurl3:i386 libasound2-plugins:i386 libgstreamer-plugins-base0.10-0:i386
