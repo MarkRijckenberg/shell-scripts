645abe336d3064f13fe3784361e9770b302a74f3
MarkRijckenberg <markrijckenberg@gmail.com>
2014-07-24 14:10:47 +0200

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -1642,5 +1642,7 @@ echo "http://static.squarespace.com/static/51156277e4b0b8b2ffe11c00/t/53ad86e5e4
 echo "http://homes.cs.washington.edu/~pedrod/papers/cacm12.pdf"
 # OSCON 2014
 echo "http://www.oscon.com/oscon2014/public/schedule/detail/37713"
+# Strata Oreilly conference
+echo "http://strataconf.com/strata2014/public/schedule/proceedings"
 # Data Day Texas conference
 echo "http://datadaytexas.com/"
