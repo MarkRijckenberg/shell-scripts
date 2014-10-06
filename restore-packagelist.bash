28cc7b6d795a1b6d63b1e12e523512e652ee722a
MarkRijckenberg <markrijckenberg@gmail.com>
2014-10-06 21:06:55 +0200

    Update restore-packagelist.bash

--- restore-packagelist.bash
+++ restore-packagelist.bash
@@ -228,6 +228,7 @@ dpkg -b foo icaclient_amd64_fixed_for_14.04_LTS.deb
 sudo dpkg -i icaclient_amd64_fixed_for_14.04_LTS.deb
 sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts
 sudo c_rehash /opt/Citrix/ICAClient/keystore/cacerts/
+xdg-mime default wfica.desktop application/x-ica
 
 # Thank you Michael May for reminding me to add the following step:
 # Click on the “open menu” icon in the top right corner of the Mozilla Firefox interface.
