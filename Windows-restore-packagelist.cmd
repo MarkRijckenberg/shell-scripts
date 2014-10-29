b005f3c357fc089a92e9eeec27d167e40fa18797
MarkRijckenberg <markrijckenberg@gmail.com>
2014-10-29 19:24:47 +0100

    Create Windows-restore-packagelist.cmd

---
+++ Windows-restore-packagelist.cmd
@@ -0,0 +1,59 @@
+REM run following commands in Windows command line (using cmd, NOT powershell) with administrator privileges:
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install powershell4"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install chocolatey"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ChocolateyGUI"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install GoogleChrome"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Wget"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install curl"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install R.Project"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install R.Studio"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install javaruntime"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip.install"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install flashplayeractivex"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Firefox"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install flashplayerplugin"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install FoxitReader"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install vlc"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install PowerShell"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ccleaner"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip.commandline"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install filezilla"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install dropbox"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install skype"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install DotNet4.5"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install paint.net"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install virtualbox"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install libreoffice"
+ 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install windirstat"
