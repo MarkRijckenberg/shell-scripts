2e412ff96c893f9fa33cec5e586f6b23482cba25
MarkRijckenberg <markrijckenberg@gmail.com>
2014-10-29 20:57:09 +0100

    Update Windows-restore-packagelist.cmd

--- Windows-restore-packagelist.cmd
+++ Windows-restore-packagelist.cmd
@@ -1,3 +1,5 @@
+REM Following .cmd script has been tested and works in Windows 8.1 Pro 64-bit:
+
 REM Please run following commands in Windows command line (using cmd, NOT powershell) as administrator:
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
