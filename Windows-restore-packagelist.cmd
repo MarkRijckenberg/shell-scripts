0ed1f64fcd8db54acac9eef56e34811795051821
MarkRijckenberg <markrijckenberg@gmail.com>
2014-10-29 20:27:16 +0100

    Update Windows-restore-packagelist.cmd

--- Windows-restore-packagelist.cmd
+++ Windows-restore-packagelist.cmd
@@ -14,7 +14,9 @@ REM Please run following commands in Windows command line (using cmd, NOT powers
  
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Wget"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install curl"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "wget http://www.confusedbycode.com/curl/curl-7.38.0-win64-local.msi"
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "msiexec /quiet /passive curl-7.38.0-win64-local.msi"
  
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install R.Project"
  
