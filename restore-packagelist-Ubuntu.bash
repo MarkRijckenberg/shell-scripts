38457f5981fbb8ba862ca931af07deacf1c2b3e9
MarkRijckenberg <markrijckenberg@gmail.com>
2014-10-31 20:24:38 +0100

    Update restore-packagelist-Ubuntu.bash

--- restore-packagelist-Ubuntu.bash
+++ restore-packagelist-Ubuntu.bash
@@ -38,13 +38,23 @@
 
 # regarding the HP Laserjet 1020 in Berlin:
 # procedure to install printer driver for HP Laserjet 1020 without needing access to openprinting.org website:
-# HPLIP=hplip-3.14.10
+# procedure to install printer driver for HP Laserjet 1020 without needing access to openprinting.org website:
+# URL=http://sourceforge.net/projects/hplip/files/latest/download?source=files
+# sudo apt-get update
+# sudo apt-get install axel
 # cd /tmp
 # rm hplip*
-# wget http://prdownloads.sourceforge.net/hplip/$HPLIP.run
-# bash $HPLIP.run
-# wget http://hplipopensource.com/hplip-web/plugin/$HPLIP-plugin.run
-# sudo bash $HPLIP-plugin.run
+# rm download*
+# axel $URL
+# HPLIPVERSION=`head download |grep label|cut -f2 -d" "`
+# mv download hplip-$HPLIPVERSION.run
+# after running the following command, the following question will be asked:
+# 'Please choose the installation mode (a=automatic*, c=custom, q=quit) :' 
+# choose q to quit (hit <ENTER> key after hitting q key)
+# then proceed with next Terminal commands:
+# bash hplip-$HPLIPVERSION.run
+# wget http://hplipopensource.com/hplip-web/plugin/hplip-$HPLIPVERSION-plugin.run
+# sudo bash hplip-$HPLIPVERSION-plugin.run
 # hp-setup
 
 
