a33a6269695ef8c63296bb9af2eadac7c5318faf
MarkRijckenberg <markrijckenberg@gmail.com>
2014-12-27 16:46:19 +0100

    Update OSX-restore-packagelist.bash

--- OSX-restore-packagelist.bash
+++ OSX-restore-packagelist.bash
@@ -2,7 +2,7 @@
 
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-sudo nvram SystemAudioVolume=" "
+# sudo nvram SystemAudioVolume=" "
 
 
 # Install Apple CLI Dev tools, java and my preferred homebrew and cask software
