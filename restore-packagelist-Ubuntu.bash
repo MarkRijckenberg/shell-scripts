696d8bba33038f36dd0f905eef5a9695fc659db6
MarkRijckenberg <markrijckenberg@gmail.com>
2015-01-17 20:59:34 +0100

    Update restore-packagelist-Ubuntu.bash

--- restore-packagelist-Ubuntu.bash
+++ restore-packagelist-Ubuntu.bash
@@ -1662,6 +1662,8 @@ echo "http://www.openintro.org/download.php?file=os2_lab_source&referrer=/stat/l
 echo "http://yihui.name/knitr/demo/editors/"
 # Explanation on how to use .Rnw files and RStudio with knitr:
 echo "http://kbroman.github.io/knitr_knutshell/pages/latex.html"
+# Data Wrangling Cheat Sheet using dplyr and tidyr packages:
+echo "http://www.rstudio.com/resources/cheatsheets/"
 # following URL added on July 10, 2014:
 # RStudio Shiny Apps cheatsheet:
 echo "http://shiny.rstudio.com/articles/cheatsheet.html"
