7ecfcce223c356d8ddabb1bd0b1f8b319215f418
MarkRijckenberg <markrijckenberg@gmail.com>
2015-04-22 22:24:11 +0200

    Update cuda-java-truecrack-python-pip-install.bash

--- cuda-java-truecrack-python-pip-install.bash
+++ cuda-java-truecrack-python-pip-install.bash
@@ -67,7 +67,7 @@ sudo apt-get install  python-bs4 python-networkx python-skimage python-sklearn
 sudo apt-get install  python-matplotlib python-numpy python-pandas
 sudo apt-get install  python-jsonschema openbox ipython ipython-notebook python-scipy
 sudo apt-get install  spyder unp python-pip build-essential python-dev swig
-sudo pip install --upgrade setuptools
+sudo pip install --upgrade setuptools seaborn
 sudo pip install --upgrade beautifulsoup4 numpy scipy matplotlib argparse
 sudo pip install --upgrade ipython mistune networkx pandas py4j runipy
 sudo pip install --upgrade scikit-image scikit-learn scipy-data_fitting
