49ad50b4b66478feaa3f58a7b57c8776aa40b99e
MarkRijckenberg <markrijckenberg@gmail.com>
2016-02-05 20:24:23 +0100

    Update python-miniconda-for-windows-install.cmd

--- python-miniconda-for-windows-install.cmd
+++ python-miniconda-for-windows-install.cmd
@@ -22,7 +22,7 @@
     
     @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
      
-    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
+    REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
      
     @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install PowerShell"
      
