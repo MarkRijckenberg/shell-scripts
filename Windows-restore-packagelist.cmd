b086d68cb3ec2d7431aadeb0ad91d23c0de3e5ad
MarkRijckenberg <markrijckenberg@gmail.com>
2016-03-25 13:59:07 +0100

    Update Windows-restore-packagelist.cmd

--- Windows-restore-packagelist.cmd
+++ Windows-restore-packagelist.cmd
@@ -26,7 +26,7 @@ pause
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
  
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install PowerShell"
 
