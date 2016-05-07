a7140022b13102cdaf32fc70c9326cf55feeb55c
MarkRijckenberg <markrijckenberg@gmail.com>
2016-05-07 13:20:38 +0200

    Update Windows-restore-packagelist.cmd

--- Windows-restore-packagelist.cmd
+++ Windows-restore-packagelist.cmd
@@ -8,7 +8,7 @@ cd c:\temp
 
 echo "Update all currently installed choco packages:"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ChocolateyPackageUpdater"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y ChocolateyPackageUpdater"
 
 cup -y all
 
@@ -26,53 +26,53 @@ pause
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
  
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install binroot"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y binroot"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install PowerShell"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y PowerShell"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install pscx"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y pscx"
  
 REM  the chocolatey package manager currently has 15055 packages (July 31,2015)
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install chocolatey"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y chocolatey"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ChocolateyGUI"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y ChocolateyGUI"
 
 REM  the nuget package manager currently has 40065 packages (July 31,2015)
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install nuget.commandline"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y nuget.commandline"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install nugetpackageexplorer"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y nugetpackageexplorer"
 
 REM Get E-mail clients and utilities
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install thunderbird"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y thunderbird"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install mozbackup"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y mozbackup"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install outlookviewer"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y outlookviewer"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install outlookconverter"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y outlookconverter"
 
 REM Get web browsers
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Firefox"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y Firefox"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install google-chrome-x64"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y google-chrome-x64"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Opera"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y Opera"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install qupzilla"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y qupzilla"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install flashplayerplugin"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y flashplayerplugin"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install grepwin"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y grepwin"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install geforce-game-ready-driver"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y geforce-game-ready-driver"
 
 REM Get Internet download utilities:
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install Wget"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y Wget"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install git"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y git"
  
 REM ############################################################################################### 
 REM Close .cmd command line window and open a new .cmd window to run the next commands.
@@ -105,120 +105,120 @@ wget  http://www.labtestproject.com/files/sha256sum/sha256sum.exe
 
 REM Get R and RStudio
  
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install r.project"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y r.project"
  
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install R.Studio"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y R.Studio"
 
 REM show list of locally installed packages - installed using choco package manager for Windows:
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco list -lo"
  
 REM jre8 installer hangs; so line below has been disabled
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install jre8"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y jre8"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip.install"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y 7zip.install"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install cmder.portable -pre"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y cmder.portable -pre"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install flashplayeractivex"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y flashplayeractivex"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install FoxitReader"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y FoxitReader"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install belgium-eid-middleware -version 407.7466"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y belgium-eid-middleware -version 407.7466"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install alcohol52-free"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y alcohol52-free"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install daemontoolslite"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y daemontoolslite"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y 7zip"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install aimp"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y aimp"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install vlc"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y vlc"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install clipgrab"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y clipgrab"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install 7zip.commandline"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y 7zip.commandline"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install filezilla"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y filezilla"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install dropbox"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y dropbox"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install skype"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y skype"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install DotNet4.5"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y DotNet4.5"
  
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install paint.net"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y paint.net"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install virtualbox"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y virtualbox"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install VBoxGuestAdditions.install"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y VBoxGuestAdditions.install"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install virtualbox.extensionpack"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y virtualbox.extensionpack"
 
 REM install Android emulator, which depends on Virtualbox:
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install andy"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y andy"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install libreoffice"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y libreoffice"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install visioviewer2013"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y visioviewer2013"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install pdf24"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y pdf24"
  
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install windirstat"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y windirstat"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install nomacs"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y nomacs"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install cpu-z"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y cpu-z"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install duplicatecommander -pre"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y duplicatecommander -pre"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install qbittorrent"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y qbittorrent"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install steam"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y steam"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install teamviewer"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y teamviewer"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install hitmanpro"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y hitmanpro"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ccleaner"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y ccleaner"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install emsisoft-anti-malware"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y emsisoft-anti-malware"
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install combofix"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y combofix"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install vagrant"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y vagrant"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install yumi"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y yumi"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install unetbootin"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y unetbootin"
 
 REM Get device driver management utilities:
 
-REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install DellCommandUpdate"
+REM @powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y DellCommandUpdate"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install driverbooster"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y driverbooster"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install googleearth"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y googleearth"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install bleachbit"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y bleachbit"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install classic-shell"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y classic-shell"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install javaruntime"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y javaruntime"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install citrix-receiver"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y citrix-receiver"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ie11"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y ie11"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install adblockplusie"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y adblockplusie"
 
 REM CD Burning software
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install win32diskimager"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y win32diskimager"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install cdburnerxp"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y cdburnerxp"
 
 echo "Show list of locally installed packages - installed using choco package manager for Windows:"
 
@@ -237,8 +237,7 @@ echo off
 
 echo "Update all currently installed choco packages:"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install ChocolateyPackageUpdater"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install -y ChocolateyPackageUpdater"
 
 cup -y all
-
 
