4fe33963994e6dfb99d2ad1310bba3aec7e57bb2
MarkRijckenberg <markrijckenberg@gmail.com>
2016-08-19 17:33:26 +0200

    Update restore-packagelist-Ubuntu.bash

--- restore-packagelist-Ubuntu.bash
+++ restore-packagelist-Ubuntu.bash
@@ -1126,9 +1126,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge arno-iptab
 sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge ufw mono-runtime
 sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge libmono-2.0-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge  xscreensaver  xscreensaver-data gnome-screensaver
-sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge evolution-data-server-common samba
-#sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes purge gcc-4.6 gcc-4.7 gcc-4.7-base gcc-4.8 gcc-4.8-base
-sudo apt-get purge gcc-4.6 gcc-4.7 gcc-4.7-base
+sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge evolution-data-server-common samba snapd
+sudo DEBIAN_FRONTEND=noninteractive apt-get  --yes --force-yes  purge gcc-4.6 gcc-4.7 gcc-4.7-base
 
 ###############################################################################################
 #     WORKAROUNDS FOR BUGS / ISSUES / CPU / MEMORY OVERCONSUMPTION                                                            #
