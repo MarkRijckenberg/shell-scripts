6a15577260a7d43d06317230f3e1f31f373383cc
MarkRijckenberg <markrijckenberg@gmail.com>
2016-10-01 13:09:25 +0200

    Update cuda-java-truecrack-python-pip-install.bash

--- cuda-java-truecrack-python-pip-install.bash
+++ cuda-java-truecrack-python-pip-install.bash
@@ -15,7 +15,7 @@ rm -rf cuda*
 #wget --no-check-certificate   https://developer.nvidia.com/cuda-downloads
 #URL=`grep 1404 cuda-downloads |head -n 1|cut -d"'" -f8`
 #VERSION=`grep 1404 cuda-downloads |head -n 1|cut -d"'" -f8|cut -d"_" -f3|cut -d"-" -f1`
-#wget `echo $URL`
+##wget `echo $URL`
 wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
 sudo dpkg -i --force-all cuda*.deb
 sudo aptitude update
