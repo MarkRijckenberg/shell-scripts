aef4453053875e606ee6d27a9c212ebd2c2e5636
MarkRijckenberg <markrijckenberg@gmail.com>
2017-02-05 01:26:27 +0100

    Update OSX-restore-packagelist.bash

--- OSX-restore-packagelist.bash
+++ OSX-restore-packagelist.bash
@@ -41,135 +41,132 @@ brew tap samueljohn/python
 brew tap homebrew/science
 
 brew update
-#brew install brew-cask
+#brew reinstall brew-cask
 
 brew upgrade
 #brew upgrade brew-cask
 brew doctor #should say "Your system is ready to brew."
 
 brew cask fetch java
-brew cask install java
 brew cask reinstall java
-brew cask install xquartz #Pre-req for some of the brew packages
-brew cask reinstall xquartz
-brew install python --framework
-brew cask reinstall python --framework
-brew install putty gtk+
+brew cask reinstall xquartz #Pre-req for some of the brew packages
+brew reinstall python --framework
+brew reinstall putty gtk+
 
-brew install ack
+brew reinstall ack
 
 # upgrade bash shell:
-brew install bash
+brew reinstall bash
 sudo sh -c 'echo "/usr/local/bin/bash" >> /etc/shells'
 chsh -s /usr/local/bin/bash
 sudo mv /bin/bash /bin/bash-backup
 sudo ln -s /usr/local/bin/bash /bin/bash
 
-brew install byacc
-brew install cabextract
-brew install coreutils
-brew install emacs --with-cocoa
-brew install findutils
-brew install fish
-brew install flex
-brew install gawk
-brew install gcc # requires Xcode CLT (command line tools) to be installed first
-brew install git
-brew install gnu-sed
-brew install go
-#brew install ispell
-brew install jdiskreport
-brew install --HEAD --64bit julia
-brew install kdiff3
-#brew install lua52
-#brew install mercurial
-#brew install minicom
-#brew install multimarkdown
-#brew install mutt
-brew install node
-brew install offline-imap
-brew install p7zip
-brew install python
-# brew install sqlite
-brew install ssh-copy-id
-#brew install svn
-#brew install the_silver_searcher
-#brew install tmux
-brew install wget
+brew reinstall byacc
+brew reinstall cabextract
+brew reinstall coreutils
+brew reinstall emacs --with-cocoa
+brew reinstall findutils
+brew reinstall fish
+brew reinstall flex
+brew reinstall gawk
+brew reinstall gcc # requires Xcode CLT (command line tools) to be installed first
+brew reinstall git
+brew reinstall gnu-sed
+brew reinstall go
+#brew reinstall ispell
+brew reinstall jdiskreport
+brew reinstall --HEAD --64bit julia
+brew reinstall kdiff3
+#brew reinstall lua52
+#brew reinstall mercurial
+#brew reinstall minicom
+#brew reinstall multimarkdown
+#brew reinstall mutt
+brew reinstall node
+brew reinstall offline-imap
+brew reinstall p7zip
+brew reinstall python
+# brew reinstall sqlite
+brew reinstall ssh-copy-id
+#brew reinstall svn
+#brew reinstall the_silver_searcher
+#brew reinstall tmux
+brew reinstall wget
 brew remove wine winetricks
 brew cask reinstall wine-staging
-#brew install z
+#brew reinstall z
 
-brew cask install 4k-youtube-to-mp3
-#brew cask install adium
-#brew cask install adobe-air 
-#brew cask install adobe-reader
-#brew cask install anki
-#brew cask install arduino
-brew cask install atom
-#brew cask install bartender
-brew cask install caffeine
-brew cask install calibre
-brew cask install ccleaner
-brew cask install chromium
-brew cask install citrix-receiver
-#brew cask install chicken
-#brew cask install controlplane
-brew cask install dash
-#brew cask install dropbox
-brew cask install dupeguru
-#brew cask install eclipse-ide
-brew cask install evernote
-brew cask install filezilla
-brew cask install firefox
-brew cask install flash
-brew cask install flip4mac
-#brew cask install flux
-#brew cask install freeplane
-#brew cask install fritzing
-brew cask install gimp
-brew cask install gnucash
-brew cask install google-chrome
-#brew cask install google-drive
-brew cask install google-earth
-#brew cask install gpgtools
-#brew cask install iterm2
-brew cask install jdiskreport
-brew cask install kdiff3
-#brew cask install keepassx0
-brew cask install kindle
-#brew cask install legacy-keepassx
-brew cask install libreoffice
-#brew cask install mactex
-#brew cask install macvim
-#brew cask install minecraft
-#brew cask install music-manager
-#brew cask install openscad
-brew cask install paintbrush
-#brew cask install quicksilver
+brew cask reinstall 4k-youtube-to-mp3
+#brew cask reinstall adium
+#brew cask reinstall adobe-air 
+#brew cask reinstall adobe-reader
+#brew cask reinstall anki
+#brew cask reinstall arduino
+brew cask reinstall atom
+#brew cask reinstall bartender
+brew cask reinstall caffeine
+brew cask reinstall calibre
+brew cask reinstall ccleaner
+brew cask reinstall chromium
+brew cask reinstall citrix-receiver
+#brew cask reinstall chicken
+#brew cask reinstall controlplane
+brew cask reinstall dash
+#brew cask reinstall dropbox
+brew cask reinstall dupeguru
+#brew cask reinstall eclipse-ide
+brew cask reinstall evernote
+brew cask reinstall filezilla
+brew cask reinstall firefox
+brew cask reinstall flash
+brew cask reinstall flip4mac
+#brew cask reinstall flux
+#brew cask reinstall freeplane
+#brew cask reinstall fritzing
+brew cask reinstall gimp
+brew cask reinstall gnucash
+brew cask reinstall google-chrome
+#brew cask reinstall google-drive
+brew cask reinstall google-earth
+#brew cask reinstall gpgtools
+#brew cask reinstall iterm2
+brew cask reinstall jdiskreport
+brew cask reinstall kdiff3
+#brew cask reinstall keepassx0
+brew cask reinstall kindle
+#brew cask reinstall legacy-keepassx
+brew cask reinstall libreoffice
+#brew cask reinstall mactex
+#brew cask reinstall macvim
+#brew cask reinstall minecraft
+#brew cask reinstall music-manager
+#brew cask reinstall openscad
+brew cask reinstall paintbrush
+#brew cask reinstall quicksilver
 brew cask fetch r
-brew cask install r
-brew cask install rstudio
-#brew cask install second-life-viewer
-#brew cask install sketchup
-brew cask install skype
-#brew cask install slic3r
-#brew cask install smcfancontrol
-#brew cask install sourcetree
-#brew cask install steam
-brew cask install stellarium
+brew cask reinstall r
+brew cask reinstall rstudio
+#brew cask reinstall second-life-viewer
+#brew cask reinstall sketchup
+brew cask reinstall skype
+#brew cask reinstall slic3r
+#brew cask reinstall smcfancontrol
+#brew cask reinstall sourcetree
+#brew cask reinstall steam
+brew cask reinstall stellarium
 brew cask fetch teamviewer
-brew cask install teamviewer
-#brew cask install the-unarchiver
-#brew cask install thunderbird
-brew cask install transmission
-#brew cask install truecrypt71a
-brew cask install unrarx
-#brew cask install vagrant
-brew cask install virtualbox
-brew cask install vlc
-# brew cask install vmware-fusion
-# brew cask install xbmc
+brew cask reinstall teamviewer
+#brew cask reinstall the-unarchiver
+#brew cask reinstall thunderbird
+brew cask reinstall transmission
+#brew cask reinstall truecrypt71a
+brew cask reinstall unrarx
+#brew cask reinstall vagrant
+brew cask reinstall virtualbox
+brew cask reinstall vlc
+# brew cask reinstall vmware-fusion
+# brew cask reinstall xbmc
 
 brew cleanup
 brew cask cleanup
@@ -181,8 +178,8 @@ source $HOME/perl5/perlbrew/etc/bashrc
 # open new Terminal and run following commands:
 # cpan App::perlbrew
 perlbrew init
-perlbrew install perl-stable
-perlbrew install-cpanm
+perlbrew reinstall perl-stable
+perlbrew reinstall-cpanm
 cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 # cpanm install CPAN::Mini
 # minicpan -l ~/perl5/minicpan -r http://mirror.internode.on.net/pub/cpan/
@@ -217,9 +214,9 @@ pip install --upgrade pyparsing
 pip install --upgrade python-dateutil
 pip install --upgrade pep8
 
-brew install numpy
-brew install scipy
-brew install homebrew/python/matplotlib
+brew reinstall numpy
+brew reinstall scipy
+brew reinstall homebrew/python/matplotlib
 
 #Make Fish your default shell:
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
