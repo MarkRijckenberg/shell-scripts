3251ad7ed99c5442c1c2a9ecf8bd391b45fc91d3
MarkRijckenberg <markrijckenberg@gmail.com>
2017-05-25 14:41:05 +0200

    Update and rename epson-scanner-automation-program.sh to epson-scanner-automation-program.bash

--- epson-scanner-automation-program.bash
+++ epson-scanner-automation-program.bash
@@ -1,10 +1,77 @@
-#!/bin/ksh
-# TYPE: Korn Shell script.
+#!/bin/bash
+# TYPE: Bash Shell script.
 # PURPOSE: Scan gray scale documents on Epson scanner to PDF file. Can unite multiple PDF files in work directory, automatically send united PDF to your Email account, and then automatically archive them.
-# REQUIRES: ksh, scanimage, tesseract, msmtp, heirloom-mailx, imagemagick, pdfunite
-# REVISED: 20130301
+# REQUIRES: Ubuntu 16.04 LTS 64-bit or newer
+# REQUIRES: ksh sane-utils tesseract-ocr tesseract-ocr-eng tesseract-ocr-nld tesseract-ocr-fra
+# tesseract-ocr-deu msmtp heirloom-mailx imagemagick pdfunite
+# REVISED: 2017/05/25
 # AUTHOR: Mark Rijckenberg
 
+# First execute these Terminal commands to install prerequisites:
+# sudo apt update
+# sudo apt install tesseract-ocr tesseract-ocr-eng tesseract-ocr-nld tesseract-ocr-fra tesseract-ocr-deu msmtp heirloom-mailx sane-utils
+
+
+  echo "Press 1 to scan English document"
+  echo "Press 2 to scan Dutch document"
+  echo "Press 3 to scan French document"
+  echo "Press 4 to scan German document"
+  read LANG
+  
+  if [ $LANG -eq 1 ]
+    then
+    echo "English chosen"
+    LANGUAGESETTING="eng"
+  fi
+ 
+  if [ $LANG -eq 2 ]
+    then
+    echo "Dutch chosen"
+    LANGUAGESETTING="nld"
+  fi
+ 
+  if [ $LANG -eq 3 ]
+    then
+    echo "French chosen"
+    LANGUAGESETTING="fra"
+  fi
+ 
+   if [ $LANG -eq 4 ]
+     then
+     echo "German chosen"
+     LANGUAGESETTING="deu"
+   fi
+   
+   
+  echo "Press 1 to scan at 300 dpi"
+  echo "Press 2 to scan at 600 dpi"
+  echo "Press 3 to scan at 1200 dpi"
+  read RES
+  
+  if [ $RES -eq 1 ]
+    then
+    echo "300 dpi chosen"
+    RESOLUTIONSETTING="300"
+  fi
+ 
+  if [ $RES -eq 2 ]
+    then
+    echo "600 dpi chosen"
+    RESOLUTIONSETTING="600"
+  fi
+ 
+  if [ $RES -eq 3 ]
+    then
+    echo "1200 dpi chosen"
+    RESOLUTIONSETTING="1200"
+  fi
+ 
+
+ echo "Which Email address should the scanned document(s) be sent to?"
+ read EMAIL
+  
+  rm -rf /tmp/*
+  
 while [[ $ACTION -ne 3 ]]; do
   WORKDIR=/tmp
   tiffname=scannedfile.tiff
@@ -13,6 +80,9 @@ while [[ $ACTION -ne 3 ]]; do
   dateunitedpdf=$(date +"%Y_%m_%d")
   pdfquantity=$(ls $WORKDIR|grep pdf|wc -l)
   cd $WORKDIR
+  echo "Language selected: $LANGUAGESETTING"
+  echo "Resolution selected: $RESOLUTIONSETTING"
+  echo "Email address selected: $EMAIL"
   echo "Press 1 to scan single document and save it in your work directory as a PDF"
   echo "Press 2 to unite multiple PDF files in your work directory, send united PDF to your Email account, and then archive them"
   echo "Press 3 to exit shell program"  
@@ -51,7 +121,7 @@ cd /tmp
 mkdir $tmpdir
 cd $tmpdir
 echo "################## Scanning ###################"
-scanimage -x 210 -y 297 --batch=out%02d.tif --format=tiff --mode Gray --resolution 300 $SOURCE
+scanimage -x 210 -y 297 --batch=out%02d.tif --format=tiff --mode Gray --resolution $RESOLUTIONSETTING $SOURCE
  
 start=1
 cnt=1
@@ -74,7 +144,7 @@ echo "... OCRing"
 tpages="$tpages b$page"
 i=$(expr $i + 1)
 echo -n " "
-tesseract $page $page -l fra
+tesseract $page $page -l $LANGUAGESETTING
 if [ $sc -gt 1 ]
 then
 cnts=`printf %02d $cnt`
@@ -119,7 +189,7 @@ rm \.txt
 		    # First create and configure the files ~/.msmtprc  and ~/.mailrc
 		    # The file ~/.msmtprc should contain the line   set sendmail=/usr/bin/msmtp
 		    # Replace emailaddress@somewhere.com with the correct destination Email address
-		    echo "Hi, here are my scanned files from `echo $dateunitedpdf`" | mail -s `echo $dateunitedpdf``echo $pdfname` -a `echo $dateunitedpdf``echo $pdfname` emailaddress@somewhere.com
+		    echo "Hi, here are my scanned files from `echo $dateunitedpdf`" | mail -s `echo $dateunitedpdf``echo $pdfname` -a `echo $dateunitedpdf``echo $pdfname` $EMAIL
 		    mkdir `echo $dateunitedpdf`
 		    mv *.pdf `echo $dateunitedpdf`
 	           fi
@@ -129,7 +199,7 @@ rm \.txt
 		    # The file ~/.msmtprc should contain the line   set sendmail=/usr/bin/msmtp
 		    # Replace emailaddress@somewhere.com with the correct destination Email address
 		    mv *.pdf  `echo $dateunitedpdf``echo $pdfname`
-		    echo "Hi, here are my scanned files from `echo $dateunitedpdf`" | mail -s `echo $dateunitedpdf``echo $pdfname` -a `echo $dateunitedpdf``echo $pdfname` emailaddress@somewhere.com
+		    echo "Hi, here are my scanned files from `echo $dateunitedpdf`" | mail -s `echo $dateunitedpdf``echo $pdfname` -a `echo $dateunitedpdf``echo $pdfname` $EMAIL
 		    mkdir `echo $dateunitedpdf`
 		    mv *.pdf `echo $dateunitedpdf`
 	           fi
