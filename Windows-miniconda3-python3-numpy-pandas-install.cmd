c3f04e46305b3de94510be65a201d46bf526fabf
MarkRijckenberg <markrijckenberg@gmail.com>
2017-06-28 20:28:01 +0200

    Update Windows-miniconda3-python3-numpy-pandas-install.cmd

--- Windows-miniconda3-python3-numpy-pandas-install.cmd
+++ Windows-miniconda3-python3-numpy-pandas-install.cmd
@@ -1,3 +1,5 @@
+REM Run this script in Powershell with Administrator privileges
+
 REM Get this and install it:
 
 REM 64-bit Windows users:
@@ -9,6 +11,10 @@ REM 32-bit Windows users:
 REM https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86.exe
 
 REM After installing Miniconda3, run the following commands on the command line:
+
+choco feature enable -n useFipsCompliantChecksums
+
+choco install miniconda3
 
 conda update conda
 
