fe73466c4d2575bd3d0dd9d2250566a7a6bd22dd
MarkRijckenberg <markrijckenberg@gmail.com>
2017-06-28 20:22:42 +0200

    Update and rename Windows-miniconda3-python3-numpy-pandas-install.txt to Windows-miniconda3-python3-numpy-pandas-install.cmd

---
+++ Windows-miniconda3-python3-numpy-pandas-install.cmd
@@ -0,0 +1,17 @@
+REM Get this and install it:
+
+REM 64-bit Windows users:
+
+REM https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe
+
+REM 32-bit Windows users:
+
+REM https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86.exe
+
+REM After installing Miniconda3, run the following commands on the command line:
+
+conda update conda
+
+conda install numpy matplotlib scipy pandas spyder
+
+REM python
