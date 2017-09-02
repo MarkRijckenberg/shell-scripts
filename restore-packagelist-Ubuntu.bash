a181df3c2ffab71a0599565e3f6021bd7de85eec
MarkRijckenberg <markrijckenberg@gmail.com>
2017-09-02 22:53:16 +0200

    Update restore-packagelist-Ubuntu.bash

--- restore-packagelist-Ubuntu.bash
+++ restore-packagelist-Ubuntu.bash
@@ -2692,6 +2692,7 @@ echo "https://s3.amazonaws.com/assets.datacamp.com/blog_assets/PandasPythonForDa
 echo "https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Numpy_Python_Cheat_Sheet.pdf"
 echo "https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Scikit_Learn_Cheat_Sheet_Python.pdf"
 echo "http://www.datasciencefree.com/cheatsheets.html"
+echo "https://github.com/ehmatthes/pcc/releases/download/v1.0.0/beginners_python_cheat_sheet_pcc_all.pdf"
 # PySpark cheat sheet:
 echo "https://s3.amazonaws.com/assets.datacamp.com/blog_assets/PySpark_Cheat_Sheet_Python.pdf"
 # Keras deep learning library - Scikit-learn - Tensorflow GPU - python framework cheatsheet:
