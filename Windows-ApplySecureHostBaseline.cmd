148f83816faf06e177a0f72486c3735643510988
MarkRijckenberg <markrijckenberg@gmail.com>
2017-11-25 14:49:55 +0100

    Update Windows-ApplySecureHostBaseline.cmd

--- Windows-ApplySecureHostBaseline.cmd
+++ Windows-ApplySecureHostBaseline.cmd
@@ -19,9 +19,6 @@ REM  the chocolatey package manager currently has 15055 packages (July 31,2015)
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "rmdir c:\temp\Secure* -Recurse -force"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "rmdir c:\temp\LGPO* -Recurse -force"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "cd c:\temp;git clone https://github.com/iadgov/Secure-Host-Baseline"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-ChildItem -Path 'c:\temp\Secure-Host-Baseline' -Recurse -Include '*.ps1','*.psm1' | Unblock-File -Verbose"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Import-Module -Name c:\temp\Secure-Host-Baseline\Scripts\GroupPolicy.psm1"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Import-Module -Name c:\temp\Secure-Host-Baseline\Compliance\Scripts\Compliance.psm1"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "cd c:\temp ; axel https://msdnshared.blob.core.windows.net/media/TNBlogsFS/prod.evol.blogs.technet.com/telligent.evolution.components.attachments/01/4062/00/00/03/65/94/11/LGPO.zip"
 
 REM Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -33,12 +30,34 @@ REM     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 REM }
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "cd C:\temp;unzip C:\temp\LGPO.zip; mkdir c:\temp\LGPO;copy LGPO*.* c:\temp\LGPO"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Invoke-ApplySecureHostBaseline -Path 'c:\temp\Secure-Host-Baseline' -PolicyNames 'Adobe Reader','AppLocker','Certificates','Chrome','EMET','Internet Explorer','Office 2013','Office 2016','Windows','Windows Firewall' -ToolPath 'c:\temp\LGPO\lgpo.exe'"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "cd c:\temp\Secure-Host-Baseline\Compliance\Scripts"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Test-Compliance -Path 'C:\temp\Secure-Host-Baseline\Adobe Reader\Compliance\AdobeReaderDC.audit' -Verbose | Out-File ComplianceReport.txt"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Test-Compliance -Path 'C:\temp\Secure-Host-Baseline\Chrome\Compliance\GoogleChrome.audit' -Verbose >ComplianceReport.txt"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Test-Compliance -Path 'C:\temp\Secure-Host-Baseline\EMET\Compliance\EMET_5.5.audit' -Verbose >ComplianceReport.txt"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Test-Compliance -Path 'C:\temp\Secure-Host-Baseline\Internet Explorer\Compliance\InternetExplorer11.audit' -Verbose >>ComplianceReport.txt"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Test-Compliance -Path 'C:\temp\Secure-Host-Baseline\Windows\Compliance\Windows10.audit' -Verbose >ComplianceReport.txt"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Select-String -Path .\ComplianceReport.txt -Pattern 'FAILED'"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Select-String -Path .\ComplianceReport.txt -Pattern 'WARN'"
+
+REM #########################################################################################################################
+REM Run in Powershell session as Administrator :
+cd c:\temp
+Get-ChildItem -Path '.\Secure-Host-Baseline' -Recurse -Include '*.ps1','*.psm1' | Unblock-File -Verbose
+Import-Module -Name .\Secure-Host-Baseline\Scripts\GroupPolicy.psm1
+Import-Module -Name .\Secure-Host-Baseline\Compliance\Scripts\Compliance.psm1
+axel https://msdnshared.blob.core.windows.net/media/TNBlogsFS/prod.evol.blogs.technet.com/telligent.evolution.components.attachments/01/4062/00/00/03/65/94/11/LGPO.zip
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+unzip LGPO.zip .\LGPO
+
+Invoke-ApplySecureHostBaseline -Path 'c:\temp\Secure-Host-Baseline' -PolicyNames 'Adobe Reader','AppLocker','Certificates','Chrome','EMET','Internet Explorer','Office 2013','Office 2016','Windows','Windows Firewall' -ToolPath '.\LGPO\lgpo.exe'
+
+cd c:\temp\Secure-Host-Baseline\Compliance\Scripts
+Test-Compliance -Path '..\..\Adobe Reader\Compliance\AdobeReaderDC.audit' -Verbose | Out-File ComplianceReport.txt
+Test-Compliance -Path '..\..\Chrome\Compliance\GoogleChrome.audit' -Verbose >ComplianceReport.txt
+Test-Compliance -Path '..\..\EMET\Compliance\EMET_5.5.audit' -Verbose >ComplianceReport.txt
+Test-Compliance -Path '..\..\Internet Explorer\Compliance\InternetExplorer11.audit' -Verbose >>ComplianceReport.txt
+Test-Compliance -Path '..\..\Windows\Compliance\Windows10.audit' -Verbose >ComplianceReport.txt
+Select-String -Path .\ComplianceReport.txt -Pattern 'FAILED'
+Select-String -Path .\ComplianceReport.txt -Pattern 'WARN'
+
+
