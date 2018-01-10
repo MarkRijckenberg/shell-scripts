8f81fb23b87548a2895170b43914428fe150db29
MarkRijckenberg <markrijckenberg@gmail.com>
2018-01-10 16:48:25 +0100

    Update meltdown-spectre-patch-check.bash

--- meltdown-spectre-patch-check.bash
+++ meltdown-spectre-patch-check.bash
@@ -1,3 +1,10 @@
+cd
+rm -rf spectre-meltdown-checker
+git clone https://github.com/speed47/spectre-meltdown-checker.git
+cd spectre-meltdown-checker
+chmod +x spectre-meltdown-checker.sh
+sudo ./spectre-meltdown-checker.sh
+
 grep CONFIG_PAGE_TABLE_ISOLATION=y /boot/config-`uname -r` && echo "patched :)" || echo "unpatched :("
 grep cpu_insecure /proc/cpuinfo && echo "patched :)" || echo "unpatched :("
 dmesg | grep "Kernel/User page tables isolation: enabled" && echo "patched :)" || echo "unpatched :("
