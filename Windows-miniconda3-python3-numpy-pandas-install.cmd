e5175965583bd16623c0f4b283b725435c2be26f
MarkRijckenberg <markrijckenberg@gmail.com>
2018-01-11 19:37:17 +0100

    Update Windows-miniconda3-python3-numpy-pandas-install.cmd

--- Windows-miniconda3-python3-numpy-pandas-install.cmd
+++ Windows-miniconda3-python3-numpy-pandas-install.cmd
@@ -16,11 +16,35 @@ REM run the following commands with Administrator privileges
 
 REM via Start menu/Programs/Anaconda3/Anaconda command line prompt:
 
-conda install anaconda conda cython matplotlib numpy pandas scipy  sympy spyder
+conda install anaconda conda cython matplotlib numpy pandas scipy  sympy spyder pip
 
-conda update anaconda conda cython matplotlib numpy pandas scipy  sympy spyder
+conda update anaconda conda cython matplotlib numpy pandas scipy  sympy spyder pip
 
-conda update anaconda conda cython matplotlib numpy pandas scipy  sympy spyder
+conda update anaconda conda cython matplotlib numpy pandas scipy  sympy spyder pip
+
+pip install --upgrade pip dvc
+
+pip install --upgrade ipython tornado 
+
+pip install --upgrade setuptools seaborn
+
+pip install --upgrade beautifulsoup4 numpy scipy matplotlib argparse
+
+pip install --upgrade sklearn jupyter streamlink
+
+pip install --upgrade mistune networkx pandas py4j runipy socli
+
+pip install --upgrade scikit-image scikit-learn scipy-data_fitting
+
+pip install --upgrade statsmodels jsonschema pattern instantmusic
+
+pip install --upgrade fire boto3 mock pytest pytest-mock bcolz configobj 
+
+pip install --upgrade ipywidgets nibabel Pillow-SIMD qgrid tqdm traitlets 
+
+pip install --upgrade visdom filelock spectral elasticsearch
+
+pip install --upgrade tensorflow-gpu
 
 REM Check if all dependencies are installed by inspecting
 
