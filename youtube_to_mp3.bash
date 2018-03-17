03f0da8af5e5d54ac9cd2ef0e70ae3a37dfe515e
MarkRijckenberg <markrijckenberg@gmail.com>
2018-03-17 11:37:07 +0100

    Update youtube_to_mp3.bash

--- youtube_to_mp3.bash
+++ youtube_to_mp3.bash
@@ -1,5 +1,11 @@
 # Procedure to copy mp3 files from Youtube playlist to Android smartphone
-# Prerequisites: Linux, bash shell
+# Prerequisites: Ubuntu 17.10 or Ubuntu 18.04, bash shell, mp3gain
+
+# install mp3gain and aacgain
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository --yes ppa:flexiondotorg/audio
+sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install --yes --force-yes   aacgain mp3gain
+
 mkdir ~/youtube-dl
 rm ~/youtube-dl/youtube-dl
 wget https://yt-dl.org/downloads/latest/youtube-dl -O ~/youtube-dl/youtube-dl
@@ -13,5 +19,7 @@ playlistdir=$(echo $playlistname | tr -d ' '| tr -d '&')
 mkdir ~/youtube-dl/$playlistdir
 cd ~/youtube-dl/$playlistdir
 ~/youtube-dl/youtube-dl --no-check-certificate -v  --extract-audio --audio-format mp3 -i  $url
+# normalize volume
+mp3gain -r -T *.mp3
 
 # then copy mp3 files to Android smartphone using AirDroid Android application
